c42f1bef5e0a4a331a5266f3a3e234d1291503dd
Aadam Ali <ali.aadam@outlook.com>
2021-07-12 18:39:16 +0100

    added jQuery

--- js/main.js
+++ js/main.js
@@ -9,115 +9,102 @@ let secondOperand = null
 let firstOperator = null
 let result = null
 
-const buttons = document.querySelectorAll('button')
-const display = document.querySelector('#display')
+const display = $('#display')
 
-// adds event listeners for the buttons based of class names
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', function() {
-        if (buttons[i].classList.contains('operand')) {
-            inputOperand(buttons[i].textContent)
-        } else if (buttons[i].classList.contains('decimal')) {
-            inputDecimal()
-        } else if (buttons[i].classList.contains('operator')) {
-            inputOperator(buttons[i].textContent) 
-        } else if (buttons[i].classList.contains('equals')) {
-            if (secondOperand == null) {
-                if (display.textContent.length > 0) {secondOperand = display.textContent}
-                else {secondOperand = '0'}
-            }
-            inputEquals(firstOperand, secondOperand, firstOperator)
-        } else if (buttons[i].classList.contains('percent')) {
-            inputPercent(display.textContent)
-        } else if (buttons[i].classList.contains('sign')) {
-            changeSign()
-        } else if (buttons[i].classList.contains('clear')) {
-            inputClear()
-        }
-    }) 
-}
+$(".operand").click(function () { inputOperand($(this).text()) });
+$(".decimal").click(function () { inputDecimal() });
+$(".operator").click(function () { inputOperator($(this).text()) });
+$(".equals").click(function () {
+    if (secondOperand == null) {
+        if ($("#display").text().length > 0) { secondOperand = $("#display").text() }
+        else { secondOperand = '0' };
+        inputEquals(firstOperand, secondOperand, firstOperator);
+    }
+})
+$(".percent").click(function () { inputPercent($(this).text()) });
+$(".sign").click(function () { changeSign() });
+$(".clear").click(function () { inputClear() });
 
 // adds event listeners for the keyboard
-window.addEventListener("keydown", keyboardAction)
+$(window).keydown(keyboardAction)
 function keyboardAction(e) {
-    console.log(e.key)
-    if (e.key >= 0 && e.key <= 9) {inputOperand(e.key)} 
-    else if (e.key == '.') {inputDecimal()}
-    else if (e.key == '+' || e.key == '-' || e.key == '*' || e.key == '/') {inputOperator(e.key)} 
-    else if (e.key == '=' || e.key == 'Enter') {if (secondOperand == null) {
-        if (display.textContent.length > 0) {secondOperand = display.textContent}
-        else {secondOperand = '0'}} 
-    inputEquals(firstOperand, secondOperand, firstOperator)}
-    else if (e.key == '%') {inputPercent(display.textContent)}
-    else if (e.key == 'Delete') {inputClear()}
-    else if (e.key == 'Backspace') {backspace()}
+    if (e.key >= 0 && e.key <= 9) { inputOperand(e.key) }
+    else if (e.key == '.') { inputDecimal() }
+    else if (e.key == '+' || e.key == '-' || e.key == '*' || e.key == '/') { inputOperator(e.key) }
+    else if (e.key == '=' || e.key == 'Enter') {
+        if (secondOperand == null) {
+            if ($("#display").text().length > 0) { secondOperand = $("#display").text() }
+            else { secondOperand = '0' }
+        }
+        inputEquals(firstOperand, secondOperand, firstOperator)
+    }
+    else if (e.key == '%') { inputPercent($("#display").text()) }
+    else if (e.key == 'Delete') { inputClear() }
+    else if (e.key == 'Backspace') { backspace() }
 }
 
 function inputOperand(operand) {
-    if (display.textContent.length < 12) {
-        display.textContent += operand
+    if ($("#display").text().length < 12) {
+        $("#display").text($("#display").text() + operand)
     }
 }
 
 function backspace() {
-    let displayArray = display.textContent.split('')
+    let displayArray = $("#display").text().split('')
     displayArray.pop()
     result = displayArray.join('')
     if (result.length == 0) {
         inputClear()
     } else {
-        display.textContent = result
+        $("#display").text($("#display").text() + result)
     }
 }
 
 // adds decimal with check to ensure that more than one decimal cannot be inputted
 function inputDecimal() {
-    if (display.textContent.match(/\./)) {
+    if ($("#display").text().match(/\./)) {
         return false
     } else {
-        display.textContent += '.'
+        $("#display").text($("#display").text() + ".")
     }
 }
 
 function inputPercent(num) {
-    if (display.textContent == '') {
-        return
-    }
+    if ($("#display").text() == '') { return }
     a = parseFloat(num) * 100
     updateDisplay(a)
 }
 
 function inputOperator(operator) {
     if (firstOperand == null && secondOperand == null) {
-        if (display.textContent == '') {
-            return
-        } else if (display.textContent == '-') {
+        if ($("#display").text() == '') { return }
+        else if ($("#display").text() == '-') {
             firstOperand = '-1'
             firstOperator = operator
-            display.textContent = ''
+            $("#display").text("")
         } else {
-            firstOperand = display.textContent
+            firstOperand = $("#display").text()
             firstOperator = operator
-            display.textContent = ''
+            $("#display").text("")
         }
     } else if (firstOperand != null && secondOperand == null) {
         firstOperator = operator
-        display.textContent = ''
+        $("#display").text("")
     }
 }
 
 // add negative/positive functionality
 function changeSign() {
-    if (display.textContent.length == 0) {
-        display.textContent = '-'
+    if ($("#display").text().length == 0) {
+        $("#display").text("-")
     } else {
-        let displayArray = display.textContent.split('')
+        let displayArray = $("#display").text().split('')
         // negative to positve
         if (displayArray[0] == '-') {
             displayArray.shift()
             result = displayArray.join('')
             updateDisplay(result)
-        } else { 
+        } else {
             // positive to negative
             displayArray.unshift('-')
             result = displayArray.join('')
@@ -140,16 +127,16 @@ function multiply(a, b) {
 
 function divide(a, b) {
     if (b == '0') {
-        display.textContent = 'Error'
+        $("#display").text("Error")
         return null
     }
     return parseFloat(a) / parseFloat(b);
 };
 
 function inputEquals(a, b, operator) {
-    secondOperand == display.textContent
+    secondOperand == $("#display").text()
     if (operator == '+') {
-        result =  add(a, b);
+        result = add(a, b);
         updateDisplay(result)
     } else if (operator == '-') {
         result = subtract(a, b);
@@ -164,7 +151,6 @@ function inputEquals(a, b, operator) {
         result = secondOperand
         updateDisplay(result)
     }
-    console.log('Equals Inputted')
 }
 
 // updates display, using scientific notation for extremely large and extremely small numbers
@@ -172,9 +158,9 @@ function updateDisplay(num) {
     firstOperand = result
     secondOperand = null
     if (num.toString().length < 12) {
-        display.textContent = num
+        $("#display").text(num)
     } else {
-        display.textContent = Number.parseFloat(num).toPrecision(7)
+        $("#display").text(Number.parseFloat(num).toPrecision(7))
     }
 }
 
@@ -183,6 +169,5 @@ function inputClear() {
     secondOperand = null
     firstOperator = null
     result = null
-    display.textContent = ''
+    $("#display").text("")
 }
-
